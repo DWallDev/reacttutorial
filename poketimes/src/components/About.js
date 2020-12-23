6f6fce05fb252bc19604c8bc97175915a22db063
Dave Wall <me@davidwall.dev>
2020-12-23 11:23:47 +1100

    Lesson 25 - React Router

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ab reprehenderit dicta totam a provident corrupti, numquam nihil in nemo aperiam culpa iusto, atque, id tempora tenetur vel necessitatibus ex!</p>
+
+        </div>
+    )
+}
+
+export default About
---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ab reprehenderit dicta totam a provident corrupti, numquam nihil in nemo aperiam culpa iusto, atque, id tempora tenetur vel necessitatibus ex!</p>
+
+        </div>
+    )
+}
+
+export default Home
